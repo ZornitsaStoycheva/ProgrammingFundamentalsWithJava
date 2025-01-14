7227f61ed77b1a4f7949b37cc6ea2fe55a1ab8b1
ZornitsaStoycheva <113451269+ZornitsaStoycheva@users.noreply.github.com>
2025-01-14 12:09:05 -0800

    add new condition

--- ArraysExercise/TopIntegers_05.java
+++ ArraysExercise/TopIntegers_05.java
@@ -6,26 +6,23 @@ import java.util.Scanner;
 public class TopIntegers_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int[] arr = Arrays.stream(scanner.nextLine().split(" "))
+         int[] arr = Arrays.stream(scanner.nextLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
         int max = Integer.MIN_VALUE;
-        boolean isBigger = true;
+
 
         for (int i = 0; i < arr.length; i++) {
-            int firstNum = arr[i];
-            for (int j = 0; j < arr.length - 1; j++) {
-
-                if (firstNum > arr[j]) {
-                    max = firstNum;
+            boolean isBigger = true;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] <= arr[j]) {
+                    isBigger = false;
                 }
-                //System.out.print(max + " ");
             }
-//            if (!isBigger) {
-            System.out.print(max + " ");
-
+            if (isBigger) {
+                System.out.print(arr[i] + " ");
+            }
         }
     }
 }
